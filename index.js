4c76ad27be186dd91b10bd4a438a5aeb79187c7b
luisbossa <luisbossa@gmail.com>
2024-10-21 00:45:48 -0600

    ytyhy

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 // Import packages
 const express = require("express");
+const home = require("./routes/home");
 
 // Middlewares
 const app = express();
 app.use(express.json());
 
-app.use("/resources", express.static("public"));
-app.set('view engine', 'ejs');
-
 // Routes
-app.use(require("./routes/index.route"));
+app.use("/home", home);
 
 // connection
 const port = process.env.PORT || 3000;
---
+++ routes/home.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+app.get("/", (req, res) => {
+  const htmlResponse = `
+      <html>
+        <head>
+          <title>NodeJs y Express en Vercel</title>
+        </head>
+        <body>
+          <h1>Soy un proyecto Back end en vercel</h1>
+        </body>
+      </html>
+    `;
+  res.send(htmlResponse);
+});
+
+module.exports = router;
