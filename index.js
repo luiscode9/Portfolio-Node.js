fa8b31114ccc0b208c1c074d0e71ebe2ce83af84
luisbossa <luisbossa@gmail.com>
2024-10-21 00:50:18 -0600

    ttgt

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // Import packages
 const express = require("express");
-const home = require("./routes/home");
 
 // Middlewares
 const app = express();
 app.use(express.json());
 
 // Routes
-app.use("/home", home);
+app.use(require("./routes/home"));
 
 // connection
 const port = process.env.PORT || 3000;
