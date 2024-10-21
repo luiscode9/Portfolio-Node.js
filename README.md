8eb6cc604ea26f0e087235718025f7a64b19950e
luisbossa <luisbossa@gmail.com>
2024-10-21 00:35:03 -0600

    deploy

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Hosting express node project on vercel
 
 ### Video
-> https://youtu.be/vCuf62T2snY
+
 
 ### Deployed on
-> https://node-express-vercel-rose.vercel.app/home
+
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 // Import packages
 const express = require("express");
-const home = require("./routes/home");
 
 // Middlewares
 const app = express();
 app.use(express.json());
 
+app.use("/resources", express.static("public"));
+app.set('view engine', 'ejs');
+
 // Routes
-app.use("/home", home);
+app.use(require("./routes/index.route"));
 
 // connection
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`port runing in http://localhost:${port}`);
+});
