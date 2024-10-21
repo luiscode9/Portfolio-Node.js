8f694d2154ba0570dbfd15533c8b79a0974cb6a3
luisbossa <luisbossa@gmail.com>
2024-10-21 01:09:38 -0600

    frrrrrrr

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,15 +1,5 @@
 const home = (req, res) => {
-  const htmlResponse = `
-    <html>
-      <head>
-        <title>NodeJs y Express en Vercel</title>
-      </head>
-      <body>
-        <h1>Soy un proyecto Back end en vercel</h1>
-      </body>
-    </html>
-  `;
-  res.send(htmlResponse);
+  res.render('index');
 };
 
 module.exports = { home };
--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+app.set('view engine', 'ejs');
+
 // Routes
-app.use(require("./routes/home"));
+app.use(require("./routes/homeRoute"));
 
 // connection
 const port = process.env.PORT || 3000;
